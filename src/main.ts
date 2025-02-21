a179a5a0812590e5abc87a8b54c5f5c914397133
Iago Mauricio <iagomauricio7@gmail.com>
2025-02-21 10:31:26 -0300

    adicionado o swagger e feito os colaboradores

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Oceanos de plástico docs')
+    .setDescription(
+      'Essa documentação contém as rotas necessárias para se fazer o uso dessa API',
+    )
+    .setVersion('1.0')
+    .addTag('LERE')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
   console.log(`🚀 Server is running at http://localhost:${port}`);

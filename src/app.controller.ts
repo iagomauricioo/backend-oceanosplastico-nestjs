c170ac8dfa68d4ff201b85d8b6dba73b2cb68cb9
iago <iagomauricio7@gmail.com>
2025-03-07 21:10:30 -0300

    adicionando html na api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
+  @Get()
+  getHome(@Res() res: Response) {
+    const html = `
+    <!DOCTYPE html>
+    <html lang="pt">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>API Oceanos de Plástico</title>
+    </head>
+    <body>
+        <h1>API do Projeto Oceanos de Plástico!</h1>
+        <a href="https://api.oceanosdeplastico.com.br/api"><h2>Swagger UI</h2></a>
+    </body>
+    </html>
+  `;
+    return res.send(html);
+  }
 }
